4cf9bab44f3a5c7c54dca66df0d92b0966c79666
Ilya <m.s.v.inkognito@gmail.com>
2021-06-25 21:57:57 +0300

    how to download file from bot

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,13 +1,18 @@
 import telebot
-from telebot import types
+from PIL import Image, ImageDraw
+import numpy as np
 
 bot = telebot.TeleBot('1855857929:AAH-r1NKWky7sM459iIdWrlI12EvUuOyHLM')
 
+
 @bot.message_handler(content_types=['text', 'photo'])
-def get_text_messages(message):
+def get_messages(message):
     if message.photo:
+        print(message.photo)
+        # file = message.photo.get_file()
+        # file.download()
         bot.send_message(message.from_user.id, "Жди...")  # обработать фото
-    elif message.text == "/help":
+    elif message.text == "/help" or message.text == "/start":
         bot.send_message(message.from_user.id, "Пришли картинку элипса, треугольника или четырехугольника, "
                                                "а я скажу тебе что нарисовано на картинке:)")
     else:
@@ -15,3 +20,7 @@ def get_text_messages(message):
 
 
 bot.polling(none_stop=True, interval=0)
+
+
+def image_to_array():
+    pass

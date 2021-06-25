e4f8abbe39dd8aacac74b5c76170513fb2ac79e8
Ilya <m.s.v.inkognito@gmail.com>
2021-06-25 23:06:09 +0300

    img resized and grayscaled as an numpy array

--- TelegramBot.py
+++ TelegramBot.py
@@ -2,15 +2,20 @@ import telebot
 from PIL import Image, ImageDraw
 import numpy as np
 
-bot = telebot.TeleBot('1855857929:AAH-r1NKWky7sM459iIdWrlI12EvUuOyHLM')
+token = '1855857929:AAH-r1NKWky7sM459iIdWrlI12EvUuOyHLM'
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(content_types=['text', 'photo'])
 def get_messages(message):
     if message.photo:
-        print(message.photo)
-        # file = message.photo.get_file()
-        # file.download()
+        file_id = message.photo[2].file_id
+        file_info = bot.get_file(file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open('tmp.jpg', 'wb') as new_file:
+            new_file.write(downloaded_file)
+        img = np.array(Image.open('tmp.jpg').resize((64, 64)).convert('L'))
+        Image.fromarray(img).save('temp.jpg')
         bot.send_message(message.from_user.id, "Жди...")  # обработать фото
     elif message.text == "/help" or message.text == "/start":
         bot.send_message(message.from_user.id, "Пришли картинку элипса, треугольника или четырехугольника, "

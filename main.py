f20879a72439f9b2dc2e688394644297fda3ed0b
Ilya <m.s.v.inkognito@gmail.com>
2021-06-26 11:13:37 +0300

    bug hot fix

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from ImgShapeBuilder import ImgShapeBuilder
 from ShapeClassifier import ShapeClassifier
 
 isb = ImgShapeBuilder("data")
-isb.generate_all(1000)
+isb.generate_all(100)
 clf = ShapeClassifier()
 clf.learn("data")
